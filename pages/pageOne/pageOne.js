56e73bc1720a24aedcc2d525c50f19430f0657a7
susha12 <87581584+susha12@users.noreply.github.com>
2022-05-27 10:12:44 +0400

    Add files via upload

---
+++ pages/pageOne/pageOne.js
@@ -0,0 +1,91 @@
+let menu = document.querySelector(".header_menu");
+let navBar = document.querySelector(".nav_bar");
+let bodyAfter = document.querySelector("body");
+let a = 0;
+function playMenu() {
+    menu.src = "../../images/header/menu.png";
+    menu.classList.remove("active");
+    navBar.style.display = "none";
+    navBarMeaningOne.style.display = "none";
+    navBarMeaningTwo.style.display = "none";
+    navBarMeaningThree.style.display = "none";
+    bodyAfter.classList.remove("active");
+    bodyAfter.style.zIndex = "-1000";
+    bodyAfter.style.overflow = "inherit";
+    a--;
+}
+menu.onclick = () => {
+    if (a == 0) {
+        menu.src = "../../images/header/close.png";
+        menu.classList.add("active");
+        navBar.style.display = "flex";
+        bodyAfter.classList.add("active");
+        bodyAfter.style.zIndex = "1000";
+        bodyAfter.style.overflow = "hidden";
+        a++;
+    } else if (a == 1) {
+        playMenu();
+    }
+}
+
+let listLiOne = document.querySelector(".list_li.one");
+let listLiTwo = document.querySelector(".list_li.two");
+let listLiFour = document.querySelector(".list_li.four");
+let navBarMeaningOne = document.querySelector(".nav_bar__meaning.one");
+let navBarMeaningTwo = document.querySelector(".nav_bar__meaning.two");
+let navBarMeaningThree = document.querySelector(".nav_bar__meaning.three");
+
+listLiOne.onclick = () => {
+    navBarMeaningOne.style.display = "flex";
+    navBarMeaningTwo.style.display = "none";
+    navBarMeaningThree.style.display = "none";
+}
+
+listLiTwo.onclick = () => {
+    navBarMeaningOne.style.display = "none";
+    navBarMeaningTwo.style.display = "flex";
+    navBarMeaningThree.style.display = "none";
+}
+
+listLiFour.onclick = () => {
+    navBarMeaningOne.style.display = "none";
+    navBarMeaningTwo.style.display = "none";
+    navBarMeaningThree.style.display = "flex";
+}
+
+let headerTitle = document.querySelector(".header_title");
+let boxTitleSmall = document.querySelector(".box_title__small");
+
+// title header targget
+titleName();
+window.onresize = () => {
+    titleName();
+}
+
+function titleName() {
+    if (1398 < window.innerWidth) {
+        headerTitle.innerHTML = "Роменський фаховий коледж КНЕУ";
+    } else if (1398 >= window.innerWidth) {
+        headerTitle.innerHTML = "РФК КНЕУ";
+    }
+}
+
+let ourImgs = document.querySelectorAll(".our_imgs");
+let bal = document.querySelectorAll(".bal");
+
+let bigImgLock = document.querySelector(".big_img__lock");
+let ourImgsOne = document.querySelector(".our_imgs.one");
+// box_our__content
+function plusSlides(a) {
+    console.log(ourImgs)
+    for (let i = 0; i < bal.length; i++) {
+        bal[i].style.background = 'white';
+    }
+    bal[a].style.background = 'rgba(2, 86, 52, 1)';
+    bigImgLock.src = ourImgs[a].src;
+    if (a == 2) {
+        ourImgsOne.style.display = "none";
+    } else if (a == 1) {
+        ourImgsOne.style.display = "flex";
+    }
+}
